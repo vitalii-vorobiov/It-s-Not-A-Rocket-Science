e4d5240a643b447e1954db972de219444be9d76f
Markiian Matsiuk <mmatsi@lv314.softservecom.com>
2020-10-11 00:55:24 +0300

    DragNDropForComplex

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 
 [RequireComponent(typeof(Renderer))]
@@ -8,21 +9,30 @@ public class DraggableObject : MonoBehaviour {
     private Color    color;
     private Renderer renderer;
     private bool     isSelected;
+    private PhotonView PV;
     
     private void Awake() {
         renderer = gameObject.GetComponent<Renderer>();
+        PV = gameObject.GetComponent<PhotonView>();
     }
 
+   [PunRPC]
     public void Select() {
         if (!isSelected) {
             color      = renderer.material.color;
             isSelected = true;
         }
-        renderer.material.SetColor("_Color", Color.white);
+        renderer.material.SetColor("_Color", Color.magenta);
     }
 
+    [PunRPC]
     public void Deselect() {
         isSelected = false;
         renderer.material.SetColor("_Color", color);
+    }
+
+    public void RequestOwnerChange()
+    {
+        PV.RequestOwnership();
     }
 }
