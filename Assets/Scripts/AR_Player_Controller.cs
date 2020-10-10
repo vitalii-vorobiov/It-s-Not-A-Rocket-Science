a57f6fa51ce4bc4ce0858187e8b7553d8ca7438b
Vitaliy Vorobyov <vvoro@softserveinc.com>
2020-10-11 00:56:29 +0300

    Added tool togle

--- Assets/Scripts/AR_Player_Controller.cs
+++ Assets/Scripts/AR_Player_Controller.cs
@@ -5,22 +5,37 @@ using System.IO;
 using Photon.Pun;
 using UnityEngine;
 
-public class AR_Player_Controller : MonoBehaviour
+public class AR_Player_Controller : Singleton<AR_Player_Controller>
 {
-
-    private PhotonView PV;
-    private GameObject followedGameObject;
+    private PhotonView      PV;
+    private GameObject      followedGameObject;
+    private WrenchComponent wrenchComponent;
+    private DragComponent   dragComponent;
 
     // Start is called before the first frame update
     private void Awake()
     {
-        PV = GetComponent<PhotonView>();
+        PV                 = GetComponent<PhotonView>();
+        wrenchComponent    = GetComponent<WrenchComponent>();
+        dragComponent      = GetComponent<DragComponent>();
         followedGameObject = GameObject.FindWithTag("MainCamera");
         if (PV.IsMine)
         {
             gameObject.layer = 8;
             transform.GetChild(0).gameObject.layer = 8;
         }
+    }
+
+    private void Start() {
+        if (!PV.IsMine) return;
+        wrenchComponent.enabled = false;
+        dragComponent.enabled   = true;
+    }
+
+    public void ToggleTool() {
+        if (!PV.IsMine) return;
+        dragComponent.enabled   = !dragComponent.enabled;
+        wrenchComponent.enabled = !wrenchComponent.enabled;
     }
 
     private void Update()
