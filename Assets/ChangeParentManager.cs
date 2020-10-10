5b0a262f056f6715b5f12e31e7ec1ff5aa0f7b5c
Markiian Matsiuk <mmatsi@lv314.softservecom.com>
2020-10-10 21:39:06 +0300

    bolt spawn both

---
+++ Assets/ChangeParentManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class ChangeParentManager : MonoBehaviour
+{
+    [PunRPC]
+    void SetBuldingCoreAsParent(){
+		Debug.Log ("RPC Test");
+		gameObject.transform.SetParent(GameObject.FindWithTag("BindingCore").transform);
+	}
+}
