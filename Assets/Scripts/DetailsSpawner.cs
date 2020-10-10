8362c366fab7591fd19e54b5c7153bac349b6890
Markiian Matsiuk <mmatsi@lv314.softservecom.com>
2020-10-10 21:15:53 +0300

    fix double via maaster check

--- Assets/Scripts/DetailsSpawner.cs
+++ Assets/Scripts/DetailsSpawner.cs
@@ -8,24 +8,33 @@ using UnityEngine;
 public class DetailsSpawner : MonoBehaviour
 {
     [SerializeField] private string prefabName;
-    public GameObject currentInstantiatedGO;
-    // void Start()
-    // {
-    //     currentInstantiatedGO = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabName), transform.position, Quaternion.identity);
-    // }
+    private GameObject currentInstantiatedGO;
+
+    void Start()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            currentInstantiatedGO = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabName),
+                transform.position, Quaternion.identity);
+        }
+    }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject == currentInstantiatedGO)
         {
             currentInstantiatedGO.GetComponent<Rigidbody>().isKinematic = false;
-            currentInstantiatedGO = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabName), transform.position, Quaternion.identity);
+
+            if (PhotonNetwork.IsMasterClient)
+            {
+                currentInstantiatedGO = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", prefabName),
+                    transform.position, Quaternion.identity);
+            }
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
