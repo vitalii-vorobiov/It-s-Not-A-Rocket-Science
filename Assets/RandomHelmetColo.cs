946ff596cc9b0b146df4737f1d6500b4ea29027a
Markiian Matsiuk <mmatsi@lv314.softservecom.com>
2020-10-11 12:01:36 +0300

    helmet

---
+++ Assets/RandomHelmetColo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomHelmetColo : MonoBehaviour
+{
+    void Start()
+    {
+        Color[] list = new[] { Color.blue, Color.red, Color.yellow, Color.white, Color.green };
+        gameObject.GetComponent<Renderer>().material.color = list[Random.Range(0,4)];
+    }
+
+}
